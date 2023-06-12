63d38f9fcbe828e23955907e3103938bdbf1b66c
Nicolas <nikolasdurango@gmail.com>
2023-06-12 18:33:29 -0500

    message form successfully sent, working in BD

--- contact_app/views.py
+++ contact_app/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import ContactForm
 
 # Create your views here.
 
 def contact(request):
     contact_form = ContactForm()
+
+    if request.method == 'POST':
+        contact_form = ContactForm(data=request.POST)
+        if contact_form.is_valid():
+            name = request.POST.get('name')
+            email = request.POST.get('email')
+            subject = request.POST.get('subject')
+            message = request.POST.get('message')
+
+            return redirect('/contact/?valid')
+
     return render(request, 'contact_app/contact.html', {'contact_form': contact_form})
