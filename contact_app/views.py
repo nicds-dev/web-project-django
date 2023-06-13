ff0d9e3bbc30844eabff2e75dc42f54e2a6434ee
Nicolas <nikolasdurango@gmail.com>
2023-06-12 23:44:39 -0500

    Contact form works ok, new gmail settings applied

--- contact_app/views.py
+++ contact_app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from .forms import ContactForm
+from django.http import HttpResponse
+from django.conf import settings
+from django.core.mail import send_mail, BadHeaderError
 
 # Create your views here.
 
@@ -13,7 +16,10 @@ def contact(request):
             email = request.POST.get('email')
             subject = request.POST.get('subject')
             message = request.POST.get('message')
-
+            try:
+                send_mail(subject, f'The user with name {name} sent the following message:?\n\n {message}', email, ['cuentaparaadobe2001@gmail.com'])
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
             return redirect('/contact/?valid')
 
     return render(request, 'contact_app/contact.html', {'contact_form': contact_form})
