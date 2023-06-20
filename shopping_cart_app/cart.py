a7a2cf5e46806a7d2fddd385c2eed4ee4cfdfe86
Nicolas <nikolasdurango@gmail.com>
2023-06-19 23:37:09 -0500

    cart app rest, sum and total of products ok

--- shopping_cart_app/cart.py
+++ shopping_cart_app/cart.py
@@ -4,20 +4,19 @@ class Cart():
         self.session = request.session
         self.cart = self.session.get("cart", {})
 
-    def add(self, product, quantity=1, update_quantity=False):
+    def add(self, product, quantity=1):
         product_id = str(product.id)
         if product_id not in self.cart:
             self.cart[product_id] = {
                 "product_id": product.id,
                 "name": product.title,
-                "quantity": 0,
+                "quantity": 1,
                 "price": str(product.price),
                 "image": product.image.url
             }
-        if update_quantity:
-            self.cart[product_id]["quantity"] = quantity
         else:
             self.cart[product_id]["quantity"] += quantity
+            self.cart[product_id]["price"] = (float(self.cart[product_id]["price"]) + float(product.price))
         self.save()
 
     def delete(self, product):
@@ -30,6 +29,7 @@ class Cart():
         product_id = str(product.id)
         if product_id in self.cart:
             self.cart[product_id]["quantity"] -= 1
+            self.cart[product_id]["price"] = (float(self.cart[product_id]["price"]) - float(product.price))
             if self.cart[product_id]["quantity"] <= 0:
                 self.delete(product)
         self.save()
--- shopping_cart_app/context_processor.py
+++ shopping_cart_app/context_processor.py
@@ -1,6 +1,5 @@
-def import_total_price(request):
-    total = 0
-    if request.user.is_authenticated:
-        for key, value in request.session["cart"].items():
-            total += (float(value["price"]*value["quantity"]))
+def import_total_price(request, total = 0):
+    #if request.user.is_authenticated:
+    for key, value in request.session["cart"].items():
+        total += (float(value["price"]))
     return {"import_total_price": total}
